7e1d91d39198f47178808959ccbc1d76140da610
Wendesday36 <bertazoeanna@gmail.com>
2023-11-27 09:42:22 +0100

    a kod megertese ,proba iras, nem nagyon ertem

--- src/hengerek/Henger_Program.java
+++ src/hengerek/Henger_Program.java
@@ -1,25 +1,38 @@
 package hengerek;
 
+import model.TomorHenger;
+import model.Henger;
 import java.util.List;
 
 public class Henger_Program {
 
     private List hengerek;
-    
-    public Henger_Program(){
-        
-    }
+    private Henger[] hengerek;   
+   
     
     public static void main(String[] args) {
-        
+        new Henger_Program().run();
     }
     
-    public void run(){
+    public   void run(){
+        lista();
+        int db = Henger.getHenger();
+        double atlagV = atlagTerfogat();
         
+        
+//        hengerek.add(h1);
     }
     
-    private List lista(){
-        return hengerek;
+    private void lista(){
+       hengerek = new Henger[4];
+       hengerek[0]= new TomorHenger(4,8);
+       hengerek[1] = new TomorHenger(2,8);
+       hengerek[2] = new TomorHenger(7,3);
+       hengerek[3] = new TomorHenger(5,9);
+       
+        for (Henger h : hengerek) {
+            System.out.println(h);
+        }
     }
     
     public double csovekSulya(){
--- src/model/Henger.java
+++ src/model/Henger.java
@@ -1,19 +1,15 @@
-package hengerek;
+package model;
 
-public class Henger {
-    private static int hengerDarab;
+public abstract class Henger {
+    private int hengerDarab;
     private double sugar, magassag;
 
     public Henger(double sugar, double magassag) {
         this.sugar = sugar;
         this.magassag = magassag;
     }
-    
-    public double terfogat(){
-        return sugar*2;
-    }
 
-    public static int getHengerDarab() {
+    public int getHengerDarab() {
         return hengerDarab;
     }
 
@@ -24,9 +20,15 @@ public class Henger {
     public double getMagassag() {
         return magassag;
     }
+    public double terfogat(){
+        
+        return Math.PI*sugar*sugar*magassag;
+        
+    }
 
     @Override
     public String toString() {
-        return "Henger{" + "sugar=" + sugar + ", magassag=" + magassag + '}';
+        return "Henger{" + "hengerDarab=" + hengerDarab + ", sugar=" + sugar + ", magassag=" + magassag + '}';
     }
+    
 }
